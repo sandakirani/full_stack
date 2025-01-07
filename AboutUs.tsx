1a965f4cdd64c826e35076496339a75661a76c3c
ShivaYakshi <sivayakshika17@gmail.com>
2025-01-07 22:00:15 +0530

    Started the about us page to design

---
+++ AboutUs.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../home/navbar'; 
+import Footer from '../home/Footer'; 
+import './AboutUs.css'; 
+
+
+const AboutUs: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      
+      <Footer />
+    </>
+  );
+};
+
+export default AboutUs;
