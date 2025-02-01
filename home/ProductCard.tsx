1103f7d0a4b188f656f635c07068994433742793
Supunika05 <143956466+Supunika05@users.noreply.github.com>
2025-02-01 22:28:44 +0530

    Update ProductCard.tsx

--- home/ProductCard.tsx
+++ home/ProductCard.tsx
@@ -3,22 +3,27 @@ import './ProductCard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as faHeartEmpty } from '@fortawesome/free-regular-svg-icons';
 import { faHeart as faHeartFilled } from '@fortawesome/free-solid-svg-icons';
-import { useFavorites } from '../components/FavoritesContext'; // Import the custom hook
+import { useFavorites } from '../Header/FavoritesContext'; // Import the custom hook
 
 interface ProductCardProps {
+  id: number;
   name: string;
   price: string;
   image: string;
+  onAddToWishlist: () => void;
+  onRemoveFromWishlist: () => void;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ image, name, price }) => {
+const ProductCard: React.FC<ProductCardProps> = ({ id, image, name, price, onAddToWishlist, onRemoveFromWishlist }) => {
   const [isFavorite, setIsFavorite] = useState(false);
-  const { incrementFavorites, decrementFavorites } = useFavorites(); // Use context
+  const { incrementFavorites, decrementFavorites } = useFavorites();
 
   const handleFavoriteClick = () => {
     if (isFavorite) {
+      onRemoveFromWishlist();
       decrementFavorites(); // Decrease the favorite count in context
     } else {
+      onAddToWishlist();
       incrementFavorites(); // Increase the favorite count in context
     }
     setIsFavorite(!isFavorite);
