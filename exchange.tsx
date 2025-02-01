8b6c3b0a42787eaca9ef186edd01744e75334f57
ShivaYakshi <sivayakshika17@gmail.com>
2025-02-01 17:05:39 +0530

    Started working

--- exchange.tsx
+++ exchange.tsx
@@ -3,3 +3,10 @@ import Navbar from "../home/navbar";
 import Footer from "../home/Footer";
 import "./exchange.css";
 
+const ReturnsRefunds: React.FC = () => {
+    const [openSection, setOpenSection] = useState<string | null>(null);
+  
+    const toggleSection = (section: string) => {
+      setOpenSection(openSection === section ? null : section);
+    };
+}
